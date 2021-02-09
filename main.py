5278deec9d48dd174c399fb97de6455dd5d9af2d
Ruben Villalvazo <vim.ruben@hotmail.com>
2021-02-08 23:12:51 -0300

    Estructura inicial

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame, sys
+
+pygame.init()
+screen_width = 1024
+screen_height = 576
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+    clock.tick(60)
