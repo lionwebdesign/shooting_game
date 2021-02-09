4dd24ec00ac2fdbc27e60469a6ad570797181743
Ruben Villalvazo <vim.ruben@hotmail.com>
2021-02-09 00:40:21 -0300

    Animacion general del escenario

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ agua_bg = pygame.image.load("shooting_game/media/Water_BG.png")
 nube_1 = pygame.image.load("shooting_game/media/Cloud1.png")
 nube_2 = pygame.image.load("shooting_game/media/Cloud2.png")
 
+#Animacion de escenario
+posicion_piso_y = 450
+velocidad_piso = 1
+posicion_agua_y = 460
+velocidad_agua = 2
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -21,9 +27,21 @@ while True:
             sys.exit()
 
     screen.blit(madera_bg,(0, 0))
-    screen.blit(piso_bg,(0, 450))
-    screen.blit(agua_bg,(0, 510))
-    
+
+    posicion_piso_y -= velocidad_piso 
+
+    if posicion_piso_y <= 425 or posicion_piso_y >= 490:
+        velocidad_piso *= -1
+
+    screen.blit(piso_bg,(0, posicion_piso_y))
+
+    posicion_agua_y -= velocidad_agua 
+
+    if posicion_agua_y <= 450 or posicion_agua_y >= 510:
+        velocidad_agua *= -1
+
+    screen.blit(agua_bg,(0, posicion_agua_y))
+
     screen.blit(nube_1,(0,0))
     screen.blit(nube_2,(150,screen_height/6))
     screen.blit(nube_1,(screen_width/2,100))
